1e3d3d3337ac78708a2a74cd77062f369cbcc637
juandapps <juandapps@hotmail.com>
2023-05-08 17:34:57 -0500

    ⚒️Fix linter errors

--- modules/Books.js
+++ modules/Books.js
@@ -1,25 +1,27 @@
-/* eslint-disable */
-
 class Books {
-  // Declare private field
   #library;
+
   #root;
+
   #list;
+
   #addBook;
+
   #contact;
+
   #listContainer;
+
   #addContainer;
+
   #contactContainer;
 
   constructor() {
-    this.#root = document.querySelector("#main");
-    this.#library = JSON.parse(localStorage.getItem("library")) || [];
-    this.#list = document.querySelectorAll("#nav-links li")[0];
-    this.#addBook = document.querySelectorAll("#nav-links li")[1];
-    this.#contact = document.querySelectorAll("#nav-links li")[2];
-    this.#listContainer = document.getElementById("list-container");
-    this.#addContainer = document.getElementById("main");
-    this.#contactContainer = document.getElementById("contact");
+    this.#root = document.querySelector('#main');
+    this.#library = JSON.parse(localStorage.getItem('library')) || [];
+    [this.#list, this.#addBook, this.#contact] = document.querySelectorAll('#nav-links li');
+    this.#listContainer = document.getElementById('list-container');
+    this.#addContainer = document.getElementById('main');
+    this.#contactContainer = document.getElementById('contact');
     this.createForm();
     this.addBook();
     this.deleteBook();
@@ -27,10 +29,9 @@ class Books {
     this.saveLibrary();
     this.hideNavLinks();
     this.addNav();
-    this.listNav()
+    this.listNav();
     this.contactNav();
   }
-
 
   createForm = () => {
     this.#root.innerHTML = `
@@ -50,16 +51,17 @@ class Books {
   };
 
   addBook = () => {
-    const form = document.querySelector("#form-container");
+    const form = document.querySelector('#form-container');
 
-    form.addEventListener("submit", (event) => {
+    form.addEventListener('submit', (event) => {
       event.preventDefault();
 
-      const title = document.querySelector("#title");
-      const author = document.querySelector("#author");
+      const title = document.querySelector('#title');
+      const author = document.querySelector('#author');
 
-      if (title.value.trim() === "" || author.value.trim() === "") {
-        alert("Please enter a title and author");
+      if (title.value.trim() === '' || author.value.trim() === '') {
+        // eslint-disable-next-line no-alert
+        alert('Please enter a title and author');
         return;
       }
 
@@ -69,19 +71,19 @@ class Books {
       };
 
       this.#library.push(book);
-      title.value = "";
-      author.value = "";
+      title.value = '';
+      author.value = '';
 
       this.render();
-      this.#listContainer.style.display = "none";
+      this.#listContainer.style.display = 'none';
     });
   };
 
   deleteBook = () => {
-    const libraryContainer = document.getElementById("library-container");
+    const libraryContainer = document.getElementById('library-container');
 
-    libraryContainer.addEventListener("click", (event) => {
-      if (event.target.classList.contains("remove-book")) {
+    libraryContainer.addEventListener('click', (event) => {
+      if (event.target.classList.contains('remove-book')) {
         const bookIndex = event.target.dataset.index;
         this.#library.splice(bookIndex, 1);
 
@@ -91,20 +93,20 @@ class Books {
   };
 
   saveLibrary = () => {
-    window.addEventListener("beforeunload", () => {
-      localStorage.setItem("library", JSON.stringify(this.#library));
+    window.addEventListener('beforeunload', () => {
+      localStorage.setItem('library', JSON.stringify(this.#library));
     });
   };
 
   render = () => {
-    const libraryContainer = document.getElementById("library-container");
-    const border = document.getElementById("list-container");
+    const libraryContainer = document.getElementById('library-container');
+    const border = document.getElementById('list-container');
     border.style.display = 'none';
-    libraryContainer.innerHTML = "";
+    libraryContainer.innerHTML = '';
 
     this.#library.forEach((book, index) => {
-      const bookContainer = document.createElement("div");
-      bookContainer.classList.add("book");
+      const bookContainer = document.createElement('div');
+      bookContainer.classList.add('book');
       bookContainer.innerHTML = `
         <ul class="book-items">
           <li class="book-title">"${book.title}" by ${book.author}</li>
@@ -119,41 +121,39 @@ class Books {
     });
   };
 
-  // SPA METHODS
-
   hideNavLinks = () => {
-    this.#addContainer.style.display = "none";
-    this.#contactContainer.style.display = "none";
+    this.#addContainer.style.display = 'none';
+    this.#contactContainer.style.display = 'none';
   };
 
   addNav = () => {
     this.#addBook.addEventListener('click', () => {
-      this.#listContainer.style.display = "none";
-      this.#addContainer.style.display = "flex";
-      this.#contactContainer.style.display = "none";
+      this.#listContainer.style.display = 'none';
+      this.#addContainer.style.display = 'flex';
+      this.#contactContainer.style.display = 'none';
       const update = document.querySelector('#header h2');
       update.textContent = 'Add a new book';
-    })
+    });
   };
 
   listNav = () => {
     this.#list.addEventListener('click', () => {
-      this.#listContainer.style.display = "flex";
-      this.#addContainer.style.display = "none";
-      this.#contactContainer.style.display = "none";
+      this.#listContainer.style.display = 'flex';
+      this.#addContainer.style.display = 'none';
+      this.#contactContainer.style.display = 'none';
       const update = document.querySelector('#header h2');
       update.textContent = 'All awesome books';
-    })
+    });
   };
 
   contactNav = () => {
     this.#contact.addEventListener('click', () => {
-      this.#listContainer.style.display = "none";
-      this.#addContainer.style.display = "none";
-      this.#contactContainer.style.display = "flex";
+      this.#listContainer.style.display = 'none';
+      this.#addContainer.style.display = 'none';
+      this.#contactContainer.style.display = 'flex';
       const update = document.querySelector('#header h2');
       update.textContent = 'Contact information';
-    })
+    });
 
     this.#contactContainer.innerHTML = `
             <p>Do have any questions or you just want to say "Hello"? <br> You can reach out to us!</p>
@@ -162,7 +162,7 @@ class Books {
                 <li>Our phone number: 004358653442</li>
                 <li>Our address: Streetname 22, 84503 City, Country</li>
             </ul>
-    `
+    `;
   };
 }
 
--- modules/Date.js
+++ modules/Date.js
@@ -1,13 +1,9 @@
-/* eslint-disable */
-
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
-
 class Date {
-
   constructor() {
     this.now = DateTime.now();
-    this.formatDate = this.now.toFormat("MMMM d '\' yyyy, t");
+    this.formatDate = this.now.toFormat("MMMM d '' yyyy, t");
     this.date = document.querySelector('#date');
     this.updateDate();
   }
